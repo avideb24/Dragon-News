0d24fb90aa428897089930026d352d03ef0a98e1
avideb24 <debavi24@gmail.com>
2023-10-03 13:47:10 +0600

    updated marquee and added NavBar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
 import Marquee from "react-fast-marquee";
 import Header from "./Header";
+import NavBar from "./NavBar";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            <div className="flex gap-2 items-center">
+            <div className="flex gap-2 items-center bg-[#F3F3F3] p-4">
                 <button className="btn btn-sm rounded-none bg-red-500 text-white">Latest</button>
                 <p className="text-[#403F3F] font-bold">
-                    <Marquee>
+                    <Marquee pauseOnHover={true}>
                         <span>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</span>
                         <span>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</span>
                         <span>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</span>
                     </Marquee>
                 </p>
             </div>
+            <NavBar></NavBar>
         </div>
     );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import userPic from '../images/user.png';
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-3 my-5">
+            <div></div>
+            <div className="flex justify-center gap-4 mt-3">
+                <Link to='/'>Home</Link>
+                <Link to='/'>About</Link>
+                <Link to='/'>Career</Link>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+                <img className="w-10" src={userPic} alt="user-image" />
+                <button className="btn btn-sm rounded-none text-white bg-[#403F3F]">Login</button>
+            </div>
         </div>
     );
 };
